899451f0835f7ee709017422a9aff88fe68e8c8f
acdhound <adivanov95@gmail.com>
2022-10-27 17:27:31 +0200

    fix the script: kill python processes first, remove the correct project directory

--- aws/configure.sh
+++ aws/configure.sh
@@ -6,13 +6,17 @@ PROJECT_HOME=${PROJECT_PARENT}/palettizer
 PROJECT_REPO_URL=https://github.com/acdhound/palettizer.git
 cd $PROJECT_PARENT
 
+echo "Killing existing python processes..."
+sudo killall python3
+sleep 5
+
 echo "Installing packages..."
 sudo yum -y upgrade python3
 sudo yum -y install pip
 sudo yum -y install git
 
 echo "Cloning the project into $PROJECT_HOME..."
-if [ -e PROJECT_HOME ]; then
+if [ -e $PROJECT_HOME ]; then
   sudo rm -r $PROJECT_HOME;
 fi
 sudo git clone $PROJECT_REPO_URL $PROJECT_HOME
@@ -20,10 +24,6 @@ sudo git clone $PROJECT_REPO_URL $PROJECT_HOME
 echo "Building the project..."
 cd palettizer
 sudo pip3 install .
-
-echo "Killing existing python processes..."
-sudo killall python3
-sleep 5
 
 echo "Starting the Telegram bot..."
 sudo python3 -m palettizerbot $TGBOT_TOKEN &
